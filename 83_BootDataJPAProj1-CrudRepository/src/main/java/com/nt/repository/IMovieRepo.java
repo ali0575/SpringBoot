fb30e25419fbab16ac8232429b47924a76c34085
ali0575 <mdshahreyarali@gmail.com>
2023-12-08 19:08:38 +0530

    f

--- 83_BootDataJPAProj1-CrudRepository/src/main/java/com/nt/repository/IMovieRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nt.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nt.entity.Movie;
-
-public interface IMovieRepo extends CrudRepository<Movie,Integer> {
-
-}
